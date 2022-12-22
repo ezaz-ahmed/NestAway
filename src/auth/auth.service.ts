6dfddb1227338072c2dabf0e3eec966946b22732
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-12-22 19:25:40 +0600

    Add: Prisma and migrate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	signin() {
+		return "signin";
+	}
+
+	signup() {
+		return "signup";
+	}
+}
