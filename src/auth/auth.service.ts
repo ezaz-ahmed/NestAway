c4011b400a21f7bb2086710184f0741671cc84cc
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-12-24 16:11:02 +0600

    Auth with jwt verification complete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,36 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthDto } from './dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signin(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
     try {
-      const user = this.prisma.user.create({
+      console.log(dto);
+
+      const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           hash,
         },
       });
-      return user;
+
+      return await this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'p2002') {
+        console.log(error.code);
+        if (error.code === 'P2002') {
           throw new ForbiddenException('Credentials taken');
         }
       }
@@ -41,6 +51,25 @@ export class AuthService {
 
     if (!isMatched) throw new ForbiddenException('Credentials Incorrect!');
 
-    return user;
+    return await this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe() {
+    return this.userService.getUser();
+  }
+}
