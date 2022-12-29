1ca4bc1ddb0ec3b7bc8cc9c1c085e7531db96e98
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-12-29 23:28:05 +0600

    env file remove

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
