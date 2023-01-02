9e7517a86dbc74ef6c21d020a8498d3a5ac0c583
Ezaz Ahmed <ezaz.dev@gmail.com>
2023-01-02 14:19:52 +0600

    Get User decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -6,6 +6,7 @@ import { AuthDto } from './dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   signin(@Body() dto: AuthDto) {
     return this.authService.login(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() Req: Request) {
-    return Req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
