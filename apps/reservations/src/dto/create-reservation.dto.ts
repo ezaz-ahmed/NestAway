24cfc7b52f1dd1c58e017bf26395f3652a8f8cb0
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-03-03 00:23:55 +0600

    🛡️feat[backend]: Implement reservation input validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,13 +1,21 @@
-import { Prop } from '@nestjs/mongoose';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
   userId: string;
 
-  @Prop()
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
 
-  @Prop()
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
